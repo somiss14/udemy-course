0f898a86336712e9cefe30c6002f218d08acfb7a
maciek@archlinux <mcyran1996@gmail.com>
2023-07-20 00:51:43 +0200

    lesson 32

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    h2 {
+      color: cadetblue;
+    }`]
 })
 export class AppComponent {
   name = 'maciek';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServer(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
